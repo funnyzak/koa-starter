8846b87da7682d3532a514a0473adfee408135e5
leon <silenceace@gmail.com>
2022-01-20 16:59:52 +0800

    chore: mongo db

--- config/index.sample.js
+++ config/index.sample.js
@@ -1,4 +1,35 @@
 module.exports = {
+  app: {
+    name: 'aliyun oss tranfer',
+    host: 'http://127.0.0.1:5000',
+    fileObject: {
+      expiration: 600 // oss 文件对象有效期，单位（秒）
+    },
+    /**
+     * 路由地址配置
+     */
+    route: {
+      path: {
+        // 文件上传路由路径
+        upload: '/oss/put'
+      }
+    }
+  },
+  /**
+   * 数据库配置
+   */
+  db: {
+    /**
+     * mongoDB配置
+     */
+    mongoDB: {
+      host: '127.0.0.1',
+      port: 27017,
+      db: 'oss-tranfer',
+      user: 'root',
+      password: 'abcedfg'
+    }
+  },
   aliyun: {
     /**
      * 阿里云OSS配置
