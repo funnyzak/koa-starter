31fab212f10814b9a60b891bf5da15d67251cd81
leon <silenceace@gmail.com>
2022-01-20 16:08:53 +0800

    chore: readme

---
+++ config/index.sample.js
@@ -0,0 +1,17 @@
+module.exports = {
+  aliyun: {
+    /**
+     * 阿里云OSS配置
+     */
+    oss: [
+      {
+        accessKeyId: 'accessKeyId',
+        accessKeySecret: 'accessKeySecret',
+        endpoint: 'https://oss-cn-beijing.aliyuncs.com',
+        bucket: 'abcdef',
+        region: 'oss-cn-beijing',
+        domain: 'http://oss-cn-beijing.aliyuncs.com'
+      }
+    ]
+  }
+};
