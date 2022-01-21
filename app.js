692fe6bab793a7bc63bf84e2b07ebdd03f20fe14
leon <silenceace@gmail.com>
2022-01-21 12:39:34 +0800

    feat: babel runtime

---
+++ app.js
@@ -0,0 +1,94 @@
+import path from 'path';
+import Koa from 'koa';
+import favicon from 'koa-favicon';
+import koaBody from 'koa-body';
+import serve from 'koa-static';
+import responseTime from 'koa-response-time';
+import json from 'koa-json';
+import views from 'koa-views';
+
+import config from './config';
+import router from './router';
+const app = new Koa();
+
+// load plugin
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// body parse 2 multipart
+app.use(
+  koaBody({
+    multipart: true, // 支持文件上传
+    encoding: 'utf-8',
+    jsonLimit: 1024 * 1024,
+    formLimit: 1024 * 1024,
+    patchNode: true,
+    onError: (error, context) => {
+      console.error(
+        'request error:',
+        JSON.stringify(error),
+        'context:',
+        JSON.stringify(context)
+      );
+    },
+    formidable: {
+      uploadDir: path.join(__dirname, 'public/upload/tmp/'), // 设置文件上传目录
+      keepExtensions: true, // 保持文件的后缀
+      maxFieldsSize: 100 * 1024 * 1024, // 文件上传大小
+      hash: 'md5',
+      onFileBegin: (name, file) => {
+        // 文件上传前的设置
+        console.log(`file name: ${name}`);
+        // console.log(file);
+      }
+    }
+  })
+);
+
+// static server
+app.use(
+  serve(__dirname + '/public', {
+    maxage: 5000
+  })
+);
+
+app.use(responseTime());
+
+// json pretter
+app.use(json());
+
+app.use(views(path.join(__dirname, '/views/ejs'), { extension: 'ejs' }));
+
+// use router
+router(app);
+
+// app.use(ctx => {
+// the parsed body will store in ctx.request.body
+// if nothing was parsed, body will be an empty object {}
+// ctx.body = ctx.request.body;
+
+//   ctx.body = 'hello world';
+// });
+
+app.use(async function pageNotFound(ctx) {
+  // we need to explicitly set 404 here
+  // so that koa doesn't assign 200 on body=
+  ctx.status = 404;
+
+  switch (ctx.accepts('html', 'json')) {
+    case 'html':
+      ctx.type = 'html';
+      ctx.body = '<p>Page Not Found</p>';
+      break;
+    case 'json':
+      ctx.body = {
+        message: 'Page Not Found'
+      };
+      break;
+    default:
+      ctx.type = 'text';
+      ctx.body = 'Page Not Found';
+  }
+});
+
+app.listen(config.app.port);
+console.log('app listening on port ' + config.app.port);
