6bd2c2954ed313e7dee79d94832aa213857794f5
leon <silenceace@gmail.com>
2022-01-21 12:39:50 +0800

    feat:  route

--- router.js
+++ router.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
 // document: https://github.com/ZijianHe/koa-router
 const Router = require('@koa/router');
+import service from './service/index.js';
 
 // aliyun oss router
 const ossRoute = new Router({
   prefix: '/oss'
 });
-
 ossRoute
   .put('/put/:bucket', (ctx, next) => {
     // ...
@@ -16,26 +16,25 @@ ossRoute
     ctx.body = 'aliyun oss';
   });
 
-
 // other
 const otherRoute = new Router();
-
 otherRoute
+  .put('/transfer', async (ctx, next) => {
+    console.log(ctx.request.files);
+    console.log(ctx.request.body);
+    ctx.body = JSON.stringify(ctx.request.files);
+  })
   .all('/', async (ctx, next) => {
-    // ctx.body = 'hello world';
     await ctx.render('index', {
       message: 'hello, 2022!'
     });
   });
 
-
 const useRouter = (app, router) => {
-  app
-    .use(router.routes())
-    .use(router.allowedMethods());
+  app.use(router.routes()).use(router.allowedMethods());
 };
 
-module.exports = app => {
+export default (app) => {
   useRouter(app, ossRoute);
   useRouter(app, otherRoute);
 };
