95cbf01295d4537d612ac88674b8f1039f11ffd5
Leon <silenceace@gmail.com>
2022-01-27 00:55:31 +0800

    feat: common

--- common/error-msg.js
+++ common/error-msg.js
@@ -8,6 +8,7 @@ module.exports = {
   REQUEST_ENTITY_TOO_LARGE: '请求内容过长',
   NO_PERMISSION: '无权限操作',
   METHOD_NOT_ALLOWED: '不支持此请求方法',
+  DB_ERROR: '数据库操作失败',
 
   NEED_LOGIN: '需要登录',
   USER_NOT_FOUND: '用户不存在'
