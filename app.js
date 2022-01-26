83495c6aedbf7acc8a044c40ee3ce8f2f7192dce
leon <silenceace@gmail.com>
2022-01-26 19:31:49 +0800

    feat: 环境变量读取启动端口

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 'use strict'
 
 const Koa = require('koa')
-const config = require('./config/config-development')
+const config = require('./config')
 const logger = require('./lib/logger')
 const LogType = require('./common/log-type')
+
+// 核心服务初始化
 require('./service')
 
 let app = new Koa()
+
 // session 会引入 app.js
 module.exports = app
 
-const middleware = require('./middleware')
-middleware(app)
+// 初始化中间件
+require('./middleware')(app)
 
-const server = app.listen(config.app.port, '0.0.0.0', () => {
+// 优先从环境变量启动端口
+const listenPort = process.env.PORT || config.app.port
+
+const server = app.listen(listenPort, '0.0.0.0', () => {
   logger.info({
     type: LogType.INIT,
     msg: 'Server listening on port: ' + server.address().port
