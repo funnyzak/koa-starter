1665e3af996520eb67e37037af8479dd67457cc2
leon <silenceace@gmail.com>
2022-01-26 18:47:15 +0800

    feat: mysql lib

--- models/mysql/index.js
+++ models/mysql/index.js
@@ -1,68 +1,9 @@
 'use strict'
 
-const fs = require('fs')
-const path = require('path')
-
-const { Sequelize, Op } = require('sequelize')
-
+const MySqlDbClient = require('../../lib/db/mysql')
 const config = require('../../config')
-const logger = require('../../lib/logger')
 
-const mysql = config.db.mysql
+const client = new MySqlDbClient(config.db.mysql, __dirname)
 
-const sequelize = new Sequelize(
-  mysql.database,
-  mysql.username,
-  mysql.password,
-  Object.assign(mysql.options, {})
-)
-
-let db = {}
-
-try {
-  //读取model 支持二级目录（二级目录名为 文件夹名.对象）
-  fs.readdirSync(__dirname)
-    .filter(function (file) {
-      return file.indexOf('.') !== 0 && file !== 'index.js'
-    })
-    .forEach(function (file) {
-      if (file.indexOf('.') > 0) {
-        let model = require(path.join(__dirname, file))(sequelize)
-        //如果表不存在，则创建表
-        model.sync({
-          force: false
-        })
-        db[model.name] = model
-      } else {
-        db[file] = {}
-        fs.readdirSync(path.join(__dirname, file))
-          .filter((f) => {
-            return f.indexOf('.') > 0
-          })
-          .forEach((f) => {
-            let model = require(path.join(__dirname, file, f))(sequelize)
-            //如果表不存在，则创建表
-            model.sync({
-              force: false
-            })
-            //console.log(file, model.name);
-            db[file][model.name] = model
-          })
-      }
-    })
-  logger.info(
-    `MySQL数据库初始化并启动成功。数据库信息：${JSON.stringify(
-      config.db.mysql
-    )}`
-  )
-} catch (err) {
-  logger.error({
-    msg: `MySQL数据库初始化失败，错误信息：${err.message}`,
-    stack: err.stack
-  })
-}
-
-db.sequelize = sequelize
-db.Sequelize = Sequelize
-
-module.exports = db
+module.exports.client = client
+module.exports = client.instance
