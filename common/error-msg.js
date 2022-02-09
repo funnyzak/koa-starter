ca61889dd0da6c2fcd8ab09e4c84763723bce290
leon <silenceace@gmail.com>
2022-02-09 20:55:45 +0800

    feat: error type

--- common/error-msg.js
+++ common/error-msg.js
@@ -9,6 +9,7 @@ module.exports = {
   NO_PERMISSION: '无权限操作',
   METHOD_NOT_ALLOWED: '不支持此请求方法',
   DB_ERROR: '数据库操作失败',
+  NO_REQUEST_FILES: '没有上传文件',
 
   NEED_LOGIN: '需要登录',
   USER_NOT_FOUND: '用户不存在'
