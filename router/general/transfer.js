77e901cea07461d36c3ca4babaead7db35b9fdf9
leon <silenceace@gmail.com>
2022-02-11 17:12:33 +0800

    feat: router transfer

--- router/general/transfer.js
+++ router/general/transfer.js
@@ -8,7 +8,11 @@ const { TOKEN_TYPE } = require('../../service/token')
 
 module.exports = [
   {
-    // demo: /transfer?signature=1&cloud=1?x-token=1&x-app-id=1
+    /**
+      curl --location --request PUT 'http://127.0.0.1:2058/transfer?signature=1&cloud=1&x-app-id=1&x-token=2' \
+      --form 'f=@/Users/potato/Downloads/开发人员面试评分表.pdf'
+     * 
+     */
     method: 'put',
     path: '/transfer',
     controller: TransferCtrl.transfer,
